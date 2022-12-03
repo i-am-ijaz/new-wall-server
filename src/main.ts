a745324c8d7289086a4ff8b71e54099b3769340e
ijazsharif <ijazsharif34@gmail.com>
2022-12-03 20:30:46 +0500

    swagger integrated

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+const { version, description } = require('../package.json');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('New Wall Backend')
+    .setDescription(description)
+    .setVersion(version)
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
