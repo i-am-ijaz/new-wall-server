00364bef74ed6e19451751df0a082cd0b3196b1e
ijazsharif <ijazsharif34@gmail.com>
2022-12-04 16:33:08 +0500

    create thumbnail for uploaded images

--- src/storage/storage.controller.ts
+++ src/storage/storage.controller.ts
@@ -8,58 +8,110 @@ import {
 
 import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
 import { MulterOptions } from '@nestjs/platform-express/multer/interfaces/multer-options.interface';
-
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBody, ApiConsumes, ApiTags } from '@nestjs/swagger';
 import { diskStorage } from 'multer';
 import { extname } from 'path';
 import { Config } from '../config';
+const sharp = require('sharp');
+
+export class ImageDetail {
+  url: string;
+  thumbnilUrl: string;
+}
 
 @ApiTags('Storage')
 @Controller('storage')
 export class StorageController {
   @Post('upload')
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        file: {
+          type: 'string',
+          format: 'binary',
+        },
+      },
+    },
+  })
   @UseInterceptors(
     FileInterceptor('file', {
       storage: diskStorage({
-        destination: Config.STORAGE_CONFIG.storagePath,
+        destination: Config.STORAGE_CONFIG.imagesPath,
         filename: StorageController.editFileName,
       }),
     } as MulterOptions),
   )
-  uploadedFile(@UploadedFile('file') file: Express.Multer.File) {
+  async uploadedFile(@UploadedFile('file') file: Express.Multer.File) {
+    console.log(StorageController.editFileName);
+    const thumbnil = await this.generateThumbnil(file);
+
     return {
-      baseUrl: `/storage/download/${file.originalname}`,
+      url: `/storage/download/${file.filename}`,
+      thumbnil: `/storage/download/${thumbnil}`,
     };
   }
 
   @Post('uploads')
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        files: {
+          type: 'string',
+          format: 'binary',
+        },
+      },
+    },
+  })
   @UseInterceptors(
     FilesInterceptor('files', 10, {
       storage: diskStorage({
-        destination: Config.STORAGE_CONFIG.storagePath,
+        destination: Config.STORAGE_CONFIG.imagesPath,
         filename: StorageController.editFileName,
       }),
     } as MulterOptions),
   )
-  uploadFiles(@UploadedFiles() files: Array<Express.Multer.File>) {
-    let filePaths: string[] = [];
+  async uploadFiles(@UploadedFiles() files: Array<Express.Multer.File>) {
+    let filePaths: ImageDetail[] = [];
 
     for (var index in files) {
-      filePaths.push(`/storage/download/${files[index].originalname}`);
-      files[index].filename;
+      const thumbnil = await this.generateThumbnil(files[index]);
+      filePaths.push({
+        url: `/storage/download/${files[index].filename}`,
+        thumbnilUrl: `/storage/download/${thumbnil}`,
+      });
     }
     return {
-      baseUrl: filePaths,
+      filePaths,
     };
   }
 
-  static editFileName = (
+  private async generateThumbnil(file: Express.Multer.File): Promise<string> {
+    const thumbnil = `thumbnail-${file.filename}`;
+    await sharp(file.path)
+      .resize(200, 200)
+      .toFile(`storage/images/${thumbnil}`, (err, resizeImage) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log('');
+        }
+      });
+    return thumbnil;
+  }
+
+  private static editFileName = (
     _req: any,
     file: { originalname: string },
     callback: (arg0: any, arg1: string) => void,
   ) => {
-    const name = file.originalname.split('.')[0];
     const fileExtName = extname(file.originalname);
-    callback(null, `${name}${fileExtName}`);
+    const timeInMillis = new Date().getTime();
+
+    const filename = `${timeInMillis}${fileExtName}`;
+    callback(null, filename);
   };
 }
